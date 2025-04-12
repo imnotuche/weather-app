7e96eacc48b1abff53a37e667c709f135d1a7036
npc <uchechukwuekwe714@gmail.com>
2025-04-12 16:22:45 +0100

    refine location search for relevance and show options for multiple location results Closes #1

--- script.js
+++ script.js
@@ -1,62 +1,158 @@
 const weatherDetails=document.querySelector('.weather-details');
 const searchBox=document.querySelector('.search-city');
 const searchButton=document.querySelector('.search-button');
+const optionContainer=document.querySelector('.option-container');
 const weatherIcon=document.querySelector('.weather-icon');
 const cityTemperature=document.querySelector('.temperature');
 const cityName=document.querySelector('.city-name');
 const weatherInfo=document.querySelector('.weather-info');
 const humidity=document.querySelector('.humidity');
 const windSpeed=document.querySelector('.wind-speed');
-const API_KEY="7570efae07684e95a00165420250604";
+const WEATHER_API_KEY="7570efae07684e95a00165420250604";    //https://www.weatherapi.com/
+const CITY_API_KEY="e9c0aae4d6b04f0ca196a2c40961e066";      //https://opencagedata.com/
 const error=document.querySelector('.error');
 const errorMessage=document.querySelector('.error-message');
 
-async function getCityWeather(city){
+async function getOptions(input) {
+    
+    const URL=`https://api.opencagedata.com/geocode/v1/json?q=${input.toLowerCase()}&key=${CITY_API_KEY}`;
+    const response=await fetch(URL);
 
-        const URL=`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=yes`
-        const response=await fetch(URL);
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
 
-        if (!response.ok) {
+    const data=await response.json();
 
-            if (response.status=="400"){
+    if (data.error) {
+        throw new Error(data.error.message);
+    }
 
-                throw new Error(`We cant find weather information for "${city}"`);
+    const citiesOnly = data.results.filter(loc => loc.components._type);
+    return citiesOnly;
 
-            } else throw new Error(`HTTP error! Status: ${response.status}`);
-
-        }
-
-        const data=await response.json();
-
-        if (data.error) {
-            throw new Error(data.error.message);
-        }
-
-        return data;
-       
 }
 
-async function showWeather(city) {
 
-    try {
+async function getCityWeather(latitude, longitude){
 
-        const data=await getCityWeather(city);
-        error.classList.remove('display');
-        weatherIcon.src=`${data.current.condition.icon}`;
-        cityTemperature.innerHTML=`${Math.round(data.current.temp_c)}<span>°C</span>`;
-        cityName.textContent=data.location.country.toUpperCase();
-        weatherInfo.textContent=data.current.condition.text;
-        humidity.innerHTML=`${Math.round(data.current.humidity)}<span>%</span>`;
-        windSpeed.innerHTML=`${Math.round(data.current.wind_kph)}<span>km/h</span>`;
-        weatherDetails.classList.add('display');
-        console.log(data);
+    const URL=`https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${latitude},${longitude}`
+    const response=await fetch(URL);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data=await response.json();
+
+    if (data.error) {
+        throw new Error(data.error.message);
+    }
+    return data;
+   
+}
+
+
+async function showOptions(search) {
+
+    try{
+
+        const data=await getOptions(search); 
+
+        if(data.length>1){
+            
+            optionContainer.innerHTML='';
+            weatherDetails.classList.remove('display');
+            const searchResponse=document.createElement('p');
+            searchResponse.textContent=`There are ${data.length} locations around the world named ${search.toUpperCase()}, 
+            select the one you're searching for`;
+            searchResponse.classList.add('search-response');
+            optionContainer.appendChild(searchResponse);
+            optionContainer.classList.add('display');
+            data.forEach(element => {
+
+                const userOption=document.createElement('div');
+                userOption.classList.add('user-option');
+    
+                userOption.addEventListener('click', async ()=>{
+                
+                    const weather=await getCityWeather(element.geometry.lat, element.geometry.lng);
+                    optionContainer.classList.remove('display');
+                    error.classList.remove('display');
+                    weatherIcon.src=`${weather.current.condition.icon}`;
+                    cityTemperature.innerHTML=`${Math.round(weather.current.temp_c)}<span>°C</span>`;
+                
+                    let isCelsius = true;
+                    cityTemperature.addEventListener("click", () => {
+                        if (isCelsius) {
+                            cityTemperature.innerHTML = `${Math.round(weather.current.temp_f)}<span>°F</span>`;
+                        } else {
+                            cityTemperature.innerHTML = `${Math.round(weather.current.temp_c)}<span>°C</span>`;
+                        }
+                        isCelsius = !isCelsius;
+                    });
+                
+                    cityName.textContent=(`${weather.location.region}, ${weather.location.country}`).toUpperCase();
+                    weatherInfo.textContent=weather.current.condition.text;
+                    humidity.innerHTML=`${Math.round(weather.current.humidity)}<span>%</span>`;
+                    windSpeed.innerHTML=`${Math.round(weather.current.wind_kph)}<span>km/h</span>`;
+                    weatherDetails.classList.add('display');
+            
+                })
+    
+                optionContainer.appendChild(userOption);
+    
+                const optionName=document.createElement('p');
+                optionName.classList.add('option-name');
+                optionName.textContent=element.formatted;
+                userOption.appendChild(optionName);
+    
+                const optionType=document.createElement('p');
+                optionType.classList.add('option-type');
+            
+                if (element.components._type=="country" || element.components._type=="continent"){
+                optionType.textContent=element.components._type;
+                } else optionType.textContent=`${element.components._type} in ${element.components.country}`
+    
+                userOption.appendChild(optionType);
+    
+            });
+
+        } else if (data.length<1) {
+            throw new Error(`No results for "${search}" did you spell it correctly?`);
+        } else {
+
+            optionContainer.classList.remove('display');
+            const weather=await getCityWeather(data[0].geometry.lat, data[0].geometry.lng);
+            error.classList.remove('display');
+            weatherIcon.src=`${weather.current.condition.icon}`;
+            cityTemperature.innerHTML=`${Math.round(weather.current.temp_c)}<span>°C</span>`;
+        
+            let isCelsius = true;
+            cityTemperature.addEventListener("click", () => {
+                if (isCelsius) {
+                    cityTemperature.innerHTML = `${Math.round(weather.current.temp_f)}<span>°F</span>`;
+                } else {
+                    cityTemperature.innerHTML = `${Math.round(weather.current.temp_c)}<span>°C</span>`;
+                }
+                isCelsius = !isCelsius;
+            });
+        
+
+            cityName.textContent=(`${weather.location.region}, ${weather.location.country}`).toUpperCase();
+            weatherInfo.textContent=weather.current.condition.text;
+            humidity.innerHTML=`${Math.round(weather.current.humidity)}<span>%</span>`;
+            windSpeed.innerHTML=`${Math.round(weather.current.wind_kph)}<span>km/h</span>`;
+            weatherDetails.classList.add('display');
+            
+        }
 
     } catch (err) {
-
+            
+        optionContainer.classList.remove('display');
         weatherDetails.classList.remove('display');
         errorMessage.textContent=`Error: ${err.message}`;
         error.classList.add('display');
-        console.log(err.message);
 
     }
 
@@ -79,7 +175,7 @@ async function getInput(event){
             errorMessage.textContent="Search field is empty!";
             error.classList.add('display');
      
-        } else await showWeather(city);
+        } else await showOptions(city);
         
     }
     
@@ -88,8 +184,3 @@ async function getInput(event){
 clearInput();
 searchBox.addEventListener("keyup", getInput);
 searchButton.addEventListener("click", getInput);
-
-
-
-
-
