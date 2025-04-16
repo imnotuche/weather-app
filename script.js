8d0507246bab08f1b758015420c40ed7981e7c8f
npc <uchechukwuekwe714@gmail.com>
2025-04-16 11:47:15 +0100

    feat: add approximate user location feature and loading animation
    closes #2
    
    fix: resolve error/options display conflict
    
    closes #3
    
    chore: minor UI enhancements

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+const generalContainer=document.querySelector('.general-container');
 const weatherDetails=document.querySelector('.weather-details');
 const searchBox=document.querySelector('.search-city');
 const searchButton=document.querySelector('.search-button');
+const locationButton=document.querySelector('.location');
+const loading=document.querySelector('.loading');
+const loadingMessage=document.querySelector('.loading-message');
 const optionContainer=document.querySelector('.option-container');
 const weatherIcon=document.querySelector('.weather-icon');
 const cityTemperature=document.querySelector('.temperature');
@@ -10,6 +14,7 @@ const humidity=document.querySelector('.humidity');
 const windSpeed=document.querySelector('.wind-speed');
 const WEATHER_API_KEY="7570efae07684e95a00165420250604";    //https://www.weatherapi.com/
 const CITY_API_KEY="e9c0aae4d6b04f0ca196a2c40961e066";      //https://opencagedata.com/
+const LOCATION_API_KEY="f866dcf601df4331acc8d602c14876ca";  //https://www.geoapify.com/
 const error=document.querySelector('.error');
 const errorMessage=document.querySelector('.error-message');
 
@@ -53,7 +58,21 @@ async function getCityWeather(latitude, longitude){
 }
 
 
+function showLoading(show=true){
+    if(show){
+
+        loading.classList.add('display');
+        weatherDetails.classList.remove('display');
+        optionContainer.classList.remove('display');
+        error.classList.remove('display');
+
+    } else loading.classList.remove('display');
+}
+
+
 async function showOptions(search) {
+
+    showLoading(true);
 
     try{
 
@@ -61,10 +80,12 @@ async function showOptions(search) {
 
         if(data.length>1){
             
+            showLoading(false)
             optionContainer.innerHTML='';
             weatherDetails.classList.remove('display');
+            error.classList.remove('display');
             const searchResponse=document.createElement('p');
-            searchResponse.textContent=`There are ${data.length} locations around the world named ${search.toUpperCase()}, 
+            searchResponse.innerHTML=`We found ${data.length} results for <span>${search.toUpperCase()}</span>, 
             select the one you're searching for`;
             searchResponse.classList.add('search-response');
             optionContainer.appendChild(searchResponse);
@@ -76,7 +97,9 @@ async function showOptions(search) {
     
                 userOption.addEventListener('click', async ()=>{
                 
+                    showLoading(true);
                     const weather=await getCityWeather(element.geometry.lat, element.geometry.lng);
+                    showLoading(false);
                     optionContainer.classList.remove('display');
                     error.classList.remove('display');
                     weatherIcon.src=`${weather.current.condition.icon}`;
@@ -86,8 +109,10 @@ async function showOptions(search) {
                     cityTemperature.addEventListener("click", () => {
                         if (isCelsius) {
                             cityTemperature.innerHTML = `${Math.round(weather.current.temp_f)}<span>°F</span>`;
+                            cityTemperature.title="click to display in Celsius";
                         } else {
                             cityTemperature.innerHTML = `${Math.round(weather.current.temp_c)}<span>°C</span>`;
+                            cityTemperature.title="click to display in Fahrenheit";
                         }
                         isCelsius = !isCelsius;
                     });
@@ -122,6 +147,7 @@ async function showOptions(search) {
             throw new Error(`No results for "${search}" did you spell it correctly?`);
         } else {
 
+            showLoading(false);
             optionContainer.classList.remove('display');
             const weather=await getCityWeather(data[0].geometry.lat, data[0].geometry.lng);
             error.classList.remove('display');
@@ -132,8 +158,10 @@ async function showOptions(search) {
             cityTemperature.addEventListener("click", () => {
                 if (isCelsius) {
                     cityTemperature.innerHTML = `${Math.round(weather.current.temp_f)}<span>°F</span>`;
+                    cityTemperature.title="click to display in Celsius";
                 } else {
                     cityTemperature.innerHTML = `${Math.round(weather.current.temp_c)}<span>°C</span>`;
+                    cityTemperature.title="click to display in Fahrenheit";
                 }
                 isCelsius = !isCelsius;
             });
@@ -148,7 +176,8 @@ async function showOptions(search) {
         }
 
     } catch (err) {
-            
+        
+        showLoading(false);
         optionContainer.classList.remove('display');
         weatherDetails.classList.remove('display');
         errorMessage.textContent=`Error: ${err.message}`;
@@ -163,6 +192,7 @@ function clearInput(){
     if(searchBox.value!="") searchBox.value="";
 }
 
+
 async function getInput(event){
 
     const city = searchBox.value.trim();
@@ -171,16 +201,67 @@ async function getInput(event){
 
         if (city === ""){
 
+            loading.classList.remove('display');
+            optionContainer.classList.remove('display');
             weatherDetails.classList.remove('display');
             errorMessage.textContent="Search field is empty!";
             error.classList.add('display');
      
-        } else await showOptions(city);
+        } else {
+            await showOptions(city);
+        }
         
     }
     
 }
 
+cityTemperature.title="click to display in Fahrenheit"; //initial title for city temperature
 clearInput();
 searchBox.addEventListener("keyup", getInput);
 searchButton.addEventListener("click", getInput);
+locationButton.addEventListener("click", async ()=>{
+
+try{   
+
+    clearInput();
+    showLoading(true);
+    const URL=`https://api.geoapify.com/v1/ipinfo?&apiKey=${LOCATION_API_KEY}`;
+    const response=await fetch(URL);
+    const data=await response.json();
+    const weather=await getCityWeather(data.location.latitude, data.location.longitude);
+    
+    showLoading(false);
+    optionContainer.classList.remove('display');
+    error.classList.remove('display');
+    weatherIcon.src=`${weather.current.condition.icon}`;
+    cityTemperature.innerHTML=`${Math.round(weather.current.temp_c)}<span>°C</span>`;
+
+    let isCelsius = true;
+    cityTemperature.addEventListener("click", () => {
+        if (isCelsius) {
+            cityTemperature.innerHTML = `${Math.round(weather.current.temp_f)}<span>°F</span>`;
+            cityTemperature.title="click to display in Celsius";
+        } else {
+            cityTemperature.innerHTML = `${Math.round(weather.current.temp_c)}<span>°C</span>`;
+            cityTemperature.title="click to display in Fahrenheit";
+        }
+        isCelsius = !isCelsius;
+    });
+
+    cityName.textContent=(`${weather.location.country} (approximate location)`).toUpperCase();
+    weatherInfo.textContent=weather.current.condition.text;
+    humidity.innerHTML=`${Math.round(weather.current.humidity)}<span>%</span>`;
+    windSpeed.innerHTML=`${Math.round(weather.current.wind_kph)}<span>km/h</span>`;
+    weatherDetails.classList.add('display');
+    
+}catch(err){
+
+    showLoading(false);
+    optionContainer.classList.remove('display');
+    weatherDetails.classList.remove('display');
+    errorMessage.textContent=`Error: ${err.message}`;
+    error.classList.add('display');
+
+}
+
+})
